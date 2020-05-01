7edeb48cefd64fd3563f87a561d037afdf8b8018
Marcus9512 <marcus.jonssonewerbring@gmail.com>
2020-05-01 12:43:14 +0200

    Added skeleton code

---
+++ src/Network/U_net.py
@@ -0,0 +1,29 @@
+
+def init():
+    '''
+    Initilize class by loading data and maybe preprocess
+    ASSIGN CUDAS IF POSSIBLE
+    :return:
+    '''
+
+def pre_process():
+    '''
+    If we need some preprocessing
+    :return:
+    '''
+
+def train():
+    '''
+    Train the network, maybe should be done on CUDAS
+    :return:
+    '''
+
+def calculate_accuracy():
+    '''
+    If requiered, implement this method (Maybe exist preimplemented in Pytorch?)
+    :return:
+    '''
+
+if __name__ == '__main__':
+    init()
+    train()
