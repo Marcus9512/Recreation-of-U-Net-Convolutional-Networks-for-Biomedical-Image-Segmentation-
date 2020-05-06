c836beea17f42c701ccedb931a058c8cc7a7b8e6
Love Almgren <lovealmgren@gmail.com>
2020-05-06 10:50:50 +0200

    augment fix

--- src/Data_processing/augment_data.py
+++ src/Data_processing/augment_data.py
@@ -1,15 +1,25 @@
 import elasticdeform
 import imageio
-#import numpy
+import numpy as np
 
-def augment(X):
-    print('test')
-
+def augment(X, Y, reps=1):
+    '''
+     Augments image set X with corresponding ground truth labels Y and returns the augmented images
+     '''
     # X = numpy.zeros((200, 300))
     # X[::10, ::10] = 1
 
-    # apply deformation with a random 3 x 3 grid
-    X_deformed = elasticdeform.deform_random_grid(X, sigma=10, points=3)
+    X_deformed = np.array([])
+    Y_deformed = np.array([])
 
-    imageio.imsave('test_X.png', X)
-    imageio.imsave('test_X_deformed.png', X_deformed)
+    for i in range(len(X)):
+        # apply deformation with a random 3 x 3 grid
+        for _ in range(reps):
+            [xi, yi] = elasticdeform.deform_random_grid([X[i], Y[i]], sigma=10, points=3)
+            np.append(X_deformed, xi)
+            np.append(Y_deformed, yi)
+    return [X_deformed, Y_deformed]
+
+    # imageio.imsave('test_X.png', X)
+    # imageio.imsave('test_X_deformed.png', X_deformed)
+    # imageio.imsave('test_Y_deformed.png', Y_deformed)
