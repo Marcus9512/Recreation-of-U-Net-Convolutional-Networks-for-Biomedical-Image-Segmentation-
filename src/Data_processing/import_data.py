651a880b2f8ba93bebb3b3783e5b18689ec7f99d
Marcus9512 <marcus.jonssonewerbring@gmail.com>
2020-05-07 13:54:16 +0200

    Image bug fixed

--- src/Data_processing/import_data.py
+++ src/Data_processing/import_data.py
@@ -25,8 +25,8 @@ def create_data(path, type, n_frames):
             for j in range(frame.shape[0]):
                 for k in range(frame.shape[1]):
                     frame[j,k] = img.getpixel((j, k)) / 255
-            frame = np.expand_dims(frame , 0)
-            frame[0] = 1
+            frame = np.expand_dims(frame , axis=2)
+            frame = frame.transpose((2, 0, 1))
             all_imgs.append(frame)
 
         except EOFError:
@@ -41,6 +41,6 @@ if __name__ == "__main__":
     train_labels = create_data(path_train, 'train_l', 30)
     test_volume = create_data(path_train, 'test_v', 30)
     
-    print(train_volume.shape)
+    print(train_volume)
     print(train_labels.shape)
     print(test_volume.shape)
